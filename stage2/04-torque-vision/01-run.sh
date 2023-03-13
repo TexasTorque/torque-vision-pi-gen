6c20c40397385609f2fc2bb14c9221a0a2e0398a
realSaddy <jrp30074@gmail.com>
2023-03-13 12:36:17 -0500

    force overwrite

--- stage2/04-torque-vision/01-run.sh
+++ stage2/04-torque-vision/01-run.sh
@@ -7,8 +7,8 @@ systemctl enable set_focus
 mkdir -p "${ROOTFS_DIR}/opt/torquevision"
 wget https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/libcamera-v0.0.5/libcamera-dev-0.0.12-bullseye-arm64.deb -O "${ROOTFS_DIR}/opt/torquevision/libcamera-dev-0.0.12-bullseye-arm64.deb"
 wget https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/libcamera-apps-v0.0.5/libcamera-apps-0.0.12-bullseye-arm64.deb -O "${ROOTFS_DIR}/opt/torquevision/libcamera-apps-0.0.12-bullseye-arm64.deb"
-dpkg -i "${ROOTFS_DIR}/opt/torquevision/libcamera-dev-0.0.12-bullseye-arm64.deb"
-dpkg -i "${ROOTFS_DIR}/opt/torquevision/libcamera-apps-0.0.12-bullseye-arm64.deb"
+dpkg -i --force-overwrite "${ROOTFS_DIR}/opt/torquevision/libcamera-dev-0.0.12-bullseye-arm64.deb"
+dpkg -i --force-overwrite "${ROOTFS_DIR}/opt/torquevision/libcamera-apps-0.0.12-bullseye-arm64.deb"
 ln -s "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/libcamera.so.0.0.4" "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/libcamera.so.0.0.3"
 ln -s "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/libcamera-base.so.0.0.4" "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/libcamera-base.so.0.0.3"
 EOF
