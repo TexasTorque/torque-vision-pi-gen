4b87073c7d90868a925da3b5c7419575bb4085f8
realSaddy <jrp30074@gmail.com>
2023-03-14 11:27:54 -0500

    Fix deb dependency

--- stage2/04-torque-vision/01-run.sh
+++ stage2/04-torque-vision/01-run.sh
@@ -8,7 +8,8 @@ apt-get remove -y libcamera0 python3-libcamera
 mkdir -p "${ROOTFS_DIR}/opt/torquevision"
 wget https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/libcamera-v0.0.5/libcamera-dev-0.0.12-bullseye-arm64.deb -O "${ROOTFS_DIR}/opt/torquevision/libcamera-dev-0.0.12-bullseye-arm64.deb"
 wget https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/libcamera-apps-v0.0.5/libcamera-apps-0.0.12-bullseye-arm64.deb -O "${ROOTFS_DIR}/opt/torquevision/libcamera-apps-0.0.12-bullseye-arm64.deb"
-dpkg -i "${ROOTFS_DIR}/opt/torquevision/libcamera-dev-0.0.12-bullseye-arm64.deb"
-dpkg -i "${ROOTFS_DIR}/opt/torquevision/libcamera-apps-0.0.12-bullseye-arm64.deb"
+set -e
+dpkg -i "${ROOTFS_DIR}/opt/torquevision/libcamera-dev-0.0.12-bullseye-arm64.deb" || true
+dpkg -i "${ROOTFS_DIR}/opt/torquevision/libcamera-apps-0.0.12-bullseye-arm64.deb" || true
 apt-get -f install
 EOF
