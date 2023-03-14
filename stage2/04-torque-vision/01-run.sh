28dce103b115b8499e01799b1547b2820c25f030
Jack <31329139+realSaddy@users.noreply.github.com>
2023-03-14 10:46:39 -0500

    Install arducam drivers w/ -f dependency check

--- stage2/04-torque-vision/01-run.sh
+++ stage2/04-torque-vision/01-run.sh
@@ -4,12 +4,11 @@ install -m 644 files/set_focus.service "${ROOTFS_DIR}/lib/systemd/system/"
 
 on_chroot << EOF
 systemctl enable set_focus
-# mkdir -p "${ROOTFS_DIR}/opt/torquevision"
-# wget https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/libcamera-v0.0.5/libcamera-dev-0.0.12-bullseye-arm64.deb -O "${ROOTFS_DIR}/opt/torquevision/libcamera-dev-0.0.12-bullseye-arm64.deb"
-# wget https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/libcamera-apps-v0.0.5/libcamera-apps-0.0.12-bullseye-arm64.deb -O "${ROOTFS_DIR}/opt/torquevision/libcamera-apps-0.0.12-bullseye-arm64.deb"
-# dpkg -i --force-overwrite "${ROOTFS_DIR}/opt/torquevision/libcamera-dev-0.0.12-bullseye-arm64.deb"
-# dpkg -i --force-overwrite "${ROOTFS_DIR}/opt/torquevision/libcamera-apps-0.0.12-bullseye-arm64.deb"
+apt-get remove -y libcamera0 python3-libcamera
+mkdir -p "${ROOTFS_DIR}/opt/torquevision"
+wget https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/libcamera-v0.0.5/libcamera-dev-0.0.12-bullseye-arm64.deb -O "${ROOTFS_DIR}/opt/torquevision/libcamera-dev-0.0.12-bullseye-arm64.deb"
+wget https://github.com/ArduCAM/Arducam-Pivariety-V4L2-Driver/releases/download/libcamera-apps-v0.0.5/libcamera-apps-0.0.12-bullseye-arm64.deb -O "${ROOTFS_DIR}/opt/torquevision/libcamera-apps-0.0.12-bullseye-arm64.deb"
+dpkg -i "${ROOTFS_DIR}/opt/torquevision/libcamera-dev-0.0.12-bullseye-arm64.deb"
+dpkg -i "${ROOTFS_DIR}/opt/torquevision/libcamera-apps-0.0.12-bullseye-arm64.deb"
+apt-get -f install
 EOF
-
-#install -m 644 "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/libcamera.so.0.0.4" "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/libcamera.so.0.0.3"
-#install -m 644 "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/libcamera-base.so.0.0.4" "${ROOTFS_DIR}/usr/lib/aarch64-linux-gnu/libcamera-base.so.0.0.3"
